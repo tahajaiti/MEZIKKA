8d18c64bd1a64f5488783a4bab6efef73fac5113
taha jaiti <kyojin.rar@gmail.com>
2025-03-08 22:06:51 +0000

    MEZ-6 FEAT: added a method inside the gen helper function to check wether a username exists or not

--- backend/app/Helpers/Gen.php
+++ backend/app/Helpers/Gen.php
@@ -5,11 +5,17 @@ namespace App\Helpers;
 use App\Models\Profile;
 use Illuminate\Support\Str;
 
-class Gen {
+class Gen
+{
 
+    /**
+     * Generate a unique username
+     * @param string $name
+     * @return string
+     */
 
-
-    public static function username(string $name) {
+    public static function username(string $name)
+    {
 
         $name = Str::slug($name);
 
@@ -17,18 +23,29 @@ class Gen {
         $base = [
             $name,
             substr($name, 0, 3) . rand(100, 999),
-            $name . rand(1,99),
+            $name . rand(1, 99),
             $name . Str::random(3)
         ];
 
 
-        foreach($base as $username) {
+        foreach ($base as $username) {
             if (!Profile::where("username", $username)->exists()) {
                 return $username;
             }
         }
 
         return $name . Str::random(5);
+    }
+
+    /**
+     * Check if a username exists
+     * @param string $username
+     * @return bool
+     */
+
+    public static function check(string $username)
+    {
+        return Profile::where("username", $username)->exists();
     }
 
 }
