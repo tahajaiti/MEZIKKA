bd630bf57a9cdbda16aa050c9b7f8e9bf3b62278
taha jaiti <kyojin.rar@gmail.com>
2025-03-19 21:22:53 +0000

    FEAT: implemnted the update method in the playlist service

--- backend/app/Http/Controllers/PlaylistController.php
+++ backend/app/Http/Controllers/PlaylistController.php
@@ -7,6 +7,7 @@ use App\Models\Playlist;
 use Illuminate\Http\Request;
 use App\Services\PlaylistService;
 use App\Http\Requests\PlaylistPostRequest;
+use App\Http\Requests\PlaylistUpdateRequest;
 
 class PlaylistController extends Controller
 {
@@ -48,7 +49,7 @@ class PlaylistController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(PlaylistUpdateRequest $request, Playlist $playlist)
     {
         //
     }
--- backend/app/Services/PlaylistService.php
+++ backend/app/Services/PlaylistService.php
@@ -4,7 +4,9 @@ namespace App\Services;
 
 use App\Models\Playlist;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Storage;
 use App\Http\Requests\PlaylistPostRequest;
+use App\Http\Requests\PlaylistUpdateRequest;
 
 class PlaylistService {
 
@@ -22,7 +24,23 @@ class PlaylistService {
         ]);
 
         return $playlist ? $playlist : null;
+    }
 
+    public function update(PlaylistUpdateRequest $request, Playlist $playlist): ?Playlist {
+
+
+        $path = $request->hasFile('cover_file') ? $request->file('cover_file')->store('playlist/covers', 'public') : null;
+
+        if ($playlist && $playlist->cover) {
+            Storage::disk('public')->delete($playlist->cover);
+        }
+
+        $data = $request->validated();
+        $data['cover'] = $path;
+
+        $res = $playlist->update($data);
+
+        return $res ? $playlist : null;
     }
 
 
