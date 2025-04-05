6a65962fae8089492b6ccbd7e9cf725382974d12
taha jaiti <kyojin.rar@gmail.com>
2025-04-05 20:15:24 +0000

    FEAT: updated the authInput componenet to also support toggling the password or not

--- frontend/src/components/Inputs/AuthInput.tsx
+++ frontend/src/components/Inputs/AuthInput.tsx
@@ -1,4 +1,5 @@
-import { ReactNode } from 'react';
+import { Eye, EyeOff } from 'lucide-react';
+import { ReactNode, useState } from 'react';
 
 interface AuthInputProps {
     id: string;
@@ -22,25 +23,42 @@ const AuthInput = ({
     error
 }: AuthInputProps) => {
 
+    const [isVisible, setIsVisible] = useState(false);
+
+    const realType = type === 'password' && isVisible ? 'text' : type;
+
+    const toggleVisibility = () => {
+        if (type !== 'password') return;
+        setIsVisible(!isVisible);
+    };
+
+    const realIcon =
+        type === 'password' ? (isVisible ? <Eye /> : <EyeOff />) : icon;
+
     return (
-        <div className="relative w-full mb-4">
-            <label htmlFor={id} className="sr-only">
-                {placeholder}
-            </label>
-            <input
-                id={id}
-                className="bg-[#373737] text-white p-3 py-4 w-full rounded-[2px] placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-white/20"
-                placeholder={placeholder}
-                type={type}
-                required={required}
-                value={value}
-                onChange={onChange}
-            />
-            <span className="absolute right-4 top-1/2 transform -translate-y-1/2 text-white/40 w-5 h-5">
-                {icon}
-            </span>
-            {error && <p className="text-red-500 text-sm mt-1">{error}</p>}
-        </div>
+        <>
+            <div className="relative w-full mb-4">
+                <label htmlFor={id} className="sr-only">
+                    {placeholder}
+                </label>
+                <input
+                    id={id}
+                    className={`bg-[#373737] text-white p-3 py-4 w-full rounded-[2px] placeholder-gray-400 focus:outline-none focus:ring-2 ${error ? 'focus:ring-red-500 border-red-500 border' : 'focus:ring-white/20'
+                        }`}
+                    placeholder={placeholder}
+                    type={realType}
+                    required={required}
+                    value={value}
+                    onChange={onChange}
+                />
+                <span className="absolute right-4 top-1/2 transform -translate-y-1/2 text-white/40 w-5 h-5 cursor-pointer"
+                    onClick={toggleVisibility}
+                >
+                    {realIcon}
+                </span>
+            </div>
+            {error && <p className="text-red-500 text-sm mt-[-10px]">{error}</p>}
+        </>
     );
 };
 
