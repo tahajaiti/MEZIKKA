e3cd1198a9745104940a4f45fed787c2ed5c8bc0
taha jaiti <kyojin.rar@gmail.com>
2025-04-05 19:13:19 +0000

    FIX: fixed the user type to also include his role

--- frontend/src/types/User.ts
+++ frontend/src/types/User.ts
@@ -1,8 +1,14 @@
+type Role = {
+    id: number,
+    name: string,
+}
+
 interface User {
     id: number,
     name: string,
     email: string,
-    role: string,
+    role_id: number,
+    role: Role,
     created_at: Date,
     updated_at: Date
 }
