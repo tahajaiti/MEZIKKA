099aa012db00652a74617731ece743185ae06cc0
taha jaiti <kyojin.rar@gmail.com>
2025-04-09 18:50:39 +0100

    FEAT: added the method for getall in the song service

--- frontend/src/api/services/song/service.ts
+++ frontend/src/api/services/song/service.ts
@@ -6,11 +6,13 @@ import api, { file } from "../../index";
 const createSong = (song: FormData) => api.post<Response<SongData>>('/songs', song);
 const getSongById = (id: string | number) => api.get<Response<SongData>>(`/songs/${id}`);
 const getSongFile = (path: string | number): Promise<ArrayBuffer> => file.get<ArrayBuffer>(`/files/${path}`);
+const getAll = () => api.get<Response<SongData[]>>('/songs');
 
 const songService = {
     createSong,
     getSongById,
-    getSongFile
+    getSongFile,
+    getAll
 }
 
 export default songService;
