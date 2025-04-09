5513c4598c3d95c36b16875cfabfb8218d33b74e
taha jaiti <kyojin.rar@gmail.com>
2025-04-09 14:41:00 +0100

    FEAT: improved the filecontroller, to use id instead of path

--- backend/app/Http/Controllers/FileController.php
+++ backend/app/Http/Controllers/FileController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Helpers\Res;
+use App\Models\Song;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
 
@@ -10,16 +11,32 @@ class FileController extends Controller
 {
     public function serve(Request $request, string $path){
 
-        if (!Storage::disk('public')->exists($path)) {
+        $song = Song::where('id', $path)->first();
+        if (!$song) {
             return Res::error('File not found', 404);
         }
 
-        $fullPath = Storage::disk('public')->path($path);
-        $mimeType = Storage::disk('public')->mimeType($path);
-
-        return response()->file($fullPath, [
+        $fullPath = Storage::disk('public')->path($song->file_path);
+        if (!file_exists($fullPath)) {
+            return Res::error('File not found', 404);
+        }
+        $mimeType = Storage::disk('public')->mimeType($song->file_path);
+        $response = response()->file($fullPath, [
             'Content-Type' => $mimeType,
+            'Content-Disposition' => 'inline; filename="' . basename($song->file_path) . '"',
         ]);
 
+        return $response;
+
+        // if (!Storage::disk('public')->exists($path)) {
+        //     return Res::error('File not found', 404);
+        // }
+
+        // $fullPath = Storage::disk('public')->path($path);
+        // $mimeType = Storage::disk('public')->mimeType($path);
+
+        // return response()->file($fullPath, [
+        //     'Content-Type' => $mimeType,
+        // ]);
     }
 }
