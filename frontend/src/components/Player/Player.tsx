d3006baebc186e631aa751a22d1414ce2bf2cc2f
taha jaiti <kyojin.rar@gmail.com>
2025-04-09 18:50:24 +0100

    FEAT: cleaned up the player componenet

--- frontend/src/components/Player/Player.tsx
+++ frontend/src/components/Player/Player.tsx
@@ -1,49 +1,45 @@
-import { Heart, Pause, Play, Volume2 } from 'lucide-react'
-import { RedSlider } from '../Inputs/Slider'
-import usePlayerStore from '../../stores/usePlayerStore'
-import { useGetFile, useGetSong } from '../../api/services/song/query';
-import { useEffect } from 'react';
+import { Heart, Pause, Play, Volume2 } from "lucide-react";
+import { RedSlider } from "../Inputs/Slider";
+import usePlayerStore from "../../stores/usePlayerStore";
+import { useGetSong } from "../../api/services/song/query";
+import { useEffect, useState } from "react";
+import SongData from "../../types/Song";
 // import { formatTime } from '../../util/Formatters';
 
 const Player = () => {
-  const {
-    isPlaying,
-    mount,
-    toggle,
-  } = usePlayerStore();
+  const [song, setSong] = useState<SongData | null>(null);
+  const { isPlaying, toggle, currentSong } = usePlayerStore();
+  const { data, refetch } = useGetSong(currentSong || "");
 
-  const { data, refetch } = useGetSong(5);
-  const { data: songFIle } = useGetFile(5);
-
-  
   useEffect(() => {
-    const init = async () => {
-      refetch();
-      if (songFIle){
-        await mount(songFIle);
+    refetch();
+    if (currentSong) {
+      if (data && data.data) {
+        setSong(data.data);
       }
     }
-    init();
-  }, [mount, refetch, songFIle]);
+  }, [currentSong, data, refetch]);
 
-  const handlePlay = async () => {
-    if (songFIle) await mount(songFIle);
+  const handlePlay = () => {
     toggle();
   };
 
+  if (!song) {
+    return (
+      <div className="bg-black w-full h-24 grid grid-cols-3 items-center px-4 border-t border-zinc-900 sticky bottom-0 left-0 right-0 z-50">
+        <h1 className="text-lg font-bold text-white">No song selected</h1>
+      </div>
+    );
+  }
+
   return (
     <div className="bg-black w-full h-24 grid grid-cols-3 items-center px-4 border-t border-zinc-900 sticky bottom-0 left-0 right-0 z-50">
-
       {/* Track info */}
       <div className="flex items-center gap-4">
-        <img
-          className="h-16 w-16 object-cover"
-          src="https://placehold.co/600x400/webp"
-          alt="Track cover"
-        />
+        <img className="h-16 w-16 object-cover" src="https://placehold.co/600x400/webp" alt="Track cover" />
         <div className="flex flex-col justify-center overflow-hidden">
-          <h1 className="text-lg font-bold text-white truncate max-w-[10rem]">{data?.data?.name}</h1>
-          <h2 className="text-sm text-gray-300 truncate max-w-[10rem]">{data?.data?.user.name}</h2>
+          <h1 className="text-lg font-bold text-white truncate max-w-[10rem]">{song.name}</h1>
+          <h2 className="text-sm text-gray-300 truncate max-w-[10rem]">{song.user.name}</h2>
         </div>
         <Heart className="text-white cursor-pointer hover:text-red-500 transition-all hover:fill-current ml-4" />
       </div>
@@ -51,23 +47,17 @@ const Player = () => {
       {/* Player controls */}
       <div className="flex flex-col items-center gap-2">
         {!isPlaying ? (
-          <button
-
-            onClick={handlePlay}
-            className="bg-white rounded-full p-2 hover:bg-red-500 transition-colors">
+          <button onClick={handlePlay} className="bg-white rounded-full p-2 hover:bg-red-500 transition-colors">
             <Play className="text-black fill-current h-5 w-5" />
           </button>
         ) : (
-          <button
-            onClick={handlePlay}
-            className="bg-white rounded-full p-2 hover:bg-red-500 transition-colors">
+          <button onClick={handlePlay} className="bg-white rounded-full p-2 hover:bg-red-500 transition-colors">
             <Pause className="text-black fill-current h-5 w-5" />
           </button>
         )}
         <div className="flex justify-between items-center gap-6 w-full max-w-[37rem] px-4">
           <span className="text-xs text-gray-400">1</span>
-          <RedSlider
-            size="small" defaultValue={30} />
+          <RedSlider size="small" defaultValue={30} />
           <span className="text-xs text-gray-400">1</span>
         </div>
       </div>
@@ -78,7 +68,7 @@ const Player = () => {
         <RedSlider defaultValue={50} sx={{ width: 100 }} valueLabelDisplay="auto" />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Player
+export default Player;
