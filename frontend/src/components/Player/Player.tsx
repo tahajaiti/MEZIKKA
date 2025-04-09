169ec59bbcade99c1de7d1d0ad052c77558bb87d
taha jaiti <kyojin.rar@gmail.com>
2025-04-09 14:41:32 +0100

    FIX:enhanced the player componenet to implement clean logic

--- frontend/src/components/Player/Player.tsx
+++ frontend/src/components/Player/Player.tsx
@@ -1,24 +1,27 @@
 import { Heart, Pause, Play, Volume2 } from 'lucide-react'
 import { RedSlider } from '../Inputs/Slider'
 import usePlayerStore from '../../stores/usePlayerStore'
-import { useGetSong } from '../../api/services/song/query';
+import { useGetFile, useGetSong } from '../../api/services/song/query';
 import { useEffect } from 'react';
+// import { formatTime } from '../../util/Formatters';
 
 const Player = () => {
-  const { isPlaying, startStopPlayer, setSong } = usePlayerStore();
-  const { data, refetch } = useGetSong('8');
+  const { isPlaying, loadAudio, playAndPause } = usePlayerStore();
+  const { data, refetch } = useGetSong('5');
+  const { data: songFIle } = useGetFile('5');
+
+  useEffect(() => {
+    if (!data || !data.data || !songFIle) return;
+    loadAudio(songFIle);
+  }, [songFIle, data, loadAudio]);
 
   useEffect(() => {
     refetch();
   }, [refetch]);
 
   const handlePlay = async () => {
-    if (!data || !data.data) return;
-    const songUrl = data.data?.file_path;
-
-    setSong(songUrl);
-    await startStopPlayer();
-  }
+    playAndPause();
+  };
 
   return (
     <div className="bg-black w-full h-24 z-40 grid grid-cols-3 items-center px-4 border-t border-zinc-900">
@@ -31,8 +34,8 @@ const Player = () => {
           alt="Track cover"
         />
         <div className="flex flex-col justify-center overflow-hidden">
-          <h1 className="text-lg font-bold text-white truncate max-w-[10rem]">TRACK NAME</h1>
-          <h2 className="text-sm text-gray-300 truncate max-w-[10rem]">TRACK USER</h2>
+          <h1 className="text-lg font-bold text-white truncate max-w-[10rem]">{data?.data?.name}</h1>
+          <h2 className="text-sm text-gray-300 truncate max-w-[10rem]">{data?.data?.user.name}</h2>
         </div>
         <Heart className="text-white cursor-pointer hover:text-red-500 transition-all hover:fill-current ml-4" />
       </div>
@@ -41,6 +44,7 @@ const Player = () => {
       <div className="flex flex-col items-center gap-2">
         {!isPlaying ? (
           <button
+
             onClick={handlePlay}
             className="bg-white rounded-full p-2 hover:bg-red-500 transition-colors">
             <Play className="text-black fill-current h-5 w-5" />
@@ -53,9 +57,10 @@ const Player = () => {
           </button>
         )}
         <div className="flex justify-between items-center gap-6 w-full max-w-[37rem] px-4">
-          <span className="text-xs text-gray-400">0:00</span>
-          <RedSlider size="small" defaultValue={30} />
-          <span className="text-xs text-gray-400">1:00</span>
+          <span className="text-xs text-gray-400">1</span>
+          <RedSlider
+            size="small" defaultValue={30} />
+          <span className="text-xs text-gray-400">1</span>
         </div>
       </div>
 
