39656f0ef57a25d7a090b059228f19a14b597c63
taha jaiti <kyojin.rar@gmail.com>
2025-04-09 14:40:19 +0100

    FEAT: defined the duration and progress state in the playerstate type

--- frontend/src/types/Player.ts
+++ frontend/src/types/Player.ts
@@ -1,8 +1,11 @@
 export interface PlayerState {
     isPlaying: boolean;
     songUrl: string | null;
-    
+    duration: number;
+    progress: number;
 
     startStopPlayer: () => Promise<void>;
     setSong: (songUrl: string) => Promise<void>;
+    setProgress: (progress: number) => void;
+    updateProgress: () => void;
 }
