af4c21e9df12c2a8d32a7e8b408457953612759f
taha jaiti <kyojin.rar@gmail.com>
2025-04-10 16:08:29 +0100

    FIX: updated the mainlayout to only use the header and player

--- frontend/src/layouts/MainLayout.tsx
+++ frontend/src/layouts/MainLayout.tsx
@@ -1,21 +1,12 @@
 import { ReactNode } from 'react';
-import Aside from '../components/Global/Aside';
 import Header from '../components/Global/Header';
 import Player from '../components/Player/Player';
 
 const MainLayout: React.FC<{ children: ReactNode }> = ({ children }) => {
   return (
     <div className="h-screen w-screen overflow-hidden flex flex-col">
-      <div className="flex flex-1 overflow-hidden">
-        <Aside />
-
-        <div className="flex-1 flex flex-col h-full relative">
-          <Header />
-
-          {children}
-
-        </div>
-      </div>
+      <Header />
+      {children}
       <Player />
     </div>
   );
