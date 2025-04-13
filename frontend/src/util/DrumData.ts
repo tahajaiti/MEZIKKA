ab409fd8e9339fc0900fa20f679b648b0e176be5
taha jaiti <kyojin.rar@gmail.com>
2025-04-13 17:30:40 +0100

    FEAT: changed DrumData to include presets from our backend cdn

--- frontend/src/util/DrumData.ts
+++ frontend/src/util/DrumData.ts
@@ -1,21 +1,46 @@
 export interface DrumPad {
-    id: number;
-    name: string;
-    soundUrl: string;
-    volume: number;
-  }
+  id: number;
+  name: string;
+  soundUrl: string;
+  volume: number;
+}
+
+export interface Preset {
+  name: string;
+  sounds: DrumPad[];  
+}
 
 // volume is in decibles
 
-const DRUM_DATA: DrumPad[] = [
-    { id: 1, name: 'Kick', soundUrl: 'https://cdn.pixabay.com/download/audio/2024/08/13/audio_b31955cb8c.mp3?filename=kick-greg-232043.mp3', volume: -10 },
-    { id: 2, name: 'Snare', soundUrl: 'https://cdn.pixabay.com/download/audio/2024/09/16/audio_57c811e0c0.mp3?filename=tr808-snare-drum-241403.mp3', volume: -10 },
-    { id: 3, name: 'Hi-hat', soundUrl: 'https://cdn.pixabay.com/download/audio/2022/03/15/audio_f0f26c04e7.mp3?filename=echoed-hi-hats-89491.mp3', volume: -10 },
-    { id: 4, name: 'Crash', soundUrl: 'https://cdn.pixabay.com/download/audio/2022/03/24/audio_3eb8899ab8.mp3?filename=electronic-crash-8-100412.mp3', volume: -10 },
-    { id: 5, name: 'Percussion', soundUrl: 'https://cdn.pixabay.com/download/audio/2022/03/24/audio_3f94762948.mp3?filename=perc-screech-thinger-104898.mp3', volume: -10 },
+const API_URL = 'http://localhost:8080/cdn/presets';
+
+const trap: DrumPad[] = [
+  { id: 1, name: 'Kick', soundUrl: `${API_URL}/trap/kick.wav`, volume: -10 },
+  { id: 2, name: 'Snare', soundUrl: `${API_URL}/trap/snare.wav`, volume: -10 },
+  { id: 3, name: 'Hi-hat', soundUrl: `${API_URL}/trap/hihat.wav`, volume: -10 },
+  { id: 4, name: 'Clap', soundUrl: `${API_URL}/trap/clap.wav`, volume: -10 },
+  { id: 5, name: 'Percussion', soundUrl: `${API_URL}/trap/perc.wav`, volume: -10 },
+  { id: 6, name: '808', soundUrl: `${API_URL}/trap/808.wav`, volume: -10 },
 ]
+
+const oldSchool: DrumPad[] = [
+  { id: 1, name: 'Kick', soundUrl: `${API_URL}/old-school/kick.wav`, volume: -10 },
+  { id: 2, name: 'Snare', soundUrl: `${API_URL}/old-school/snare.wav`, volume: -10 },
+  { id: 3, name: 'Hi-hat', soundUrl: `${API_URL}/old-school/hihat.wav`, volume: -10 },
+  { id: 4, name: 'Crash', soundUrl: `${API_URL}/old-school/crash.wav`, volume: -10 },
+  { id: 5, name: 'Open-Hat', soundUrl: `${API_URL}/old-school/openhat.wav`, volume: -10 },
+  { id: 6, name: 'Bass', soundUrl: `${API_URL}/old-school/bass.wav`, volume: -10 },
+]
+
+
+
+export const PRESETS: Preset[] = [
+  { name: 'Trap', sounds: trap },
+  { name: 'Old School', sounds: oldSchool },
+];
+
 
 export const STEPS = 16;
 export const BPM = 120;
 
-export default DRUM_DATA;
+export default trap;
