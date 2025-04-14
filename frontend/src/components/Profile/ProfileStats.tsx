90d619050cd18e368281a480c4c4cb941303510e
taha jaiti <kyojin.rar@gmail.com>
2025-04-14 21:27:49 +0100

    FEAT: created the profile tsats card

---
+++ frontend/src/components/Profile/ProfileStats.tsx
@@ -0,0 +1,39 @@
+import { useGetFollows } from '../../api/services/follow/query';
+
+const ProfileStats = () => {
+    const { data, isError } = useGetFollows();
+
+    if (isError) {
+        return (
+            <div className="text-red-500">
+                An error occurred while fetching followers.
+            </div>
+        )
+    }
+    if (!data || !data.data) {
+        return (
+            <div className="text-gray-500">
+                Loading...
+            </div>
+        )
+    }
+
+    console.log(data);
+
+    return (
+        <div className="flex items-center gap-6 text-sm">
+            <div className="flex flex-col items-center">
+                <span className="text-white font-semibold">{data.data.followerCount}</span>
+                <span className="text-zinc-500">Followers</span>
+            </div>
+            <div className="w-px h-8 bg-zinc-800"></div>
+            <div className="flex flex-col items-center">
+                <span className="text-white font-semibold">{data.data.followCount}</span>
+                <span className="text-zinc-500">Following</span>
+            </div>
+            
+        </div>
+    )
+}
+
+export default ProfileStats
