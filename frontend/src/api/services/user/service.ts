dab4f484032d0919ea4ad64c659bbcf307668427
taha jaiti <kyojin.rar@gmail.com>
2025-04-14 19:41:48 +0100

    FEAT: created the updateprofile method in the user service

--- frontend/src/api/services/user/service.ts
+++ frontend/src/api/services/user/service.ts
@@ -1,12 +1,16 @@
 import api from "../..";
+import Profile from "../../../types/Profile";
 import Response from "../../../types/Response";
 import User from "../../../types/User";
 
 
 const getUserById = (id: string | number) => api.get<Response<User>>(`/users/${id}`);
+const updateProfile = (data: FormData) => api.post<Response<Profile>>(`/profile?_method=put`, data);
+
 
 const userService = {
-    getUserById
+    getUserById,
+    updateProfile
 }
 
 export default userService;
