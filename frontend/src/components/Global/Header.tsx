6ef46a509d920646c19fb04459c7004974be38d7
taha jaiti <kyojin.rar@gmail.com>
2025-04-14 19:42:08 +0100

    FEAT: updated the header to use a logout button

--- frontend/src/components/Global/Header.tsx
+++ frontend/src/components/Global/Header.tsx
@@ -1,8 +1,10 @@
-import { PlusIcon } from 'lucide-react';
+import { DoorOpen, PlusIcon } from 'lucide-react';
 import useAuthStore from '../../stores/authStore';
 import { formatUrl } from '../../util/Formatters';
 import MezikkaText from '../Texts/MezikkaText';
 import { Link } from 'react-router';
+import { logout } from '../../stores/authStore';
+
 
 const Header = () => {
   const { profile, user } = useAuthStore();
@@ -17,21 +19,29 @@ const Header = () => {
           to='/create/song'
           className="flex items-center gap-2 py-1 cursor-pointer bg-red-500 hover:bg-red-600 px-2 rounded-sm font-bold"
         >
-          <PlusIcon/>
+          <PlusIcon />
         </Link>
       </div>
 
-      <Link
-        to={`/profile/${user?.id}`}
-        className="flex items-center gap-2 py-1 px-2 cursor-pointer"
-      >
-        <img
-          src={img}
-          alt={`Avatar image for ${profile?.username}`}
-          className="w-10 h-10 rounded-full bg-zinc-800 flex items-center justify-center border border-zinc-700"
-        />
-        <span className="text-zinc-300 hidden sm:block">{profile?.username}</span>
-      </Link>
+      <div className='flex items-center gap-8'>
+        <Link
+          to={`/profile/${user?.id}`}
+          className="flex items-center gap-2 py-1 px-2 cursor-pointer"
+        >
+          <img
+            src={img}
+            alt={`Avatar image for ${profile?.username}`}
+            className="w-10 h-10 rounded-full bg-zinc-800 flex items-center justify-center border border-zinc-700"
+          />
+          <span className="text-zinc-300 hidden sm:block">{profile?.username}</span>
+        </Link>
+
+        <button
+          onClick={() => logout()}
+          className='bg-red-500 hover:bg-red-600 p-2 rounded-sm cursor-pointer'>
+          <DoorOpen />
+        </button>
+      </div>
     </header>
   );
 };
