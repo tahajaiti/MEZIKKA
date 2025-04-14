7569efa582c40e065a712e71753395f2a1fb20f9
taha jaiti <kyojin.rar@gmail.com>
2025-04-14 19:41:29 +0100

    FEAT: implemnted the useupdateprofile query to update the user profile

--- frontend/src/api/services/user/query.ts
+++ frontend/src/api/services/user/query.ts
@@ -1,14 +1,36 @@
-import { useQuery, UseQueryResult } from "@tanstack/react-query";
+import { useMutation, UseMutationResult, useQuery, useQueryClient, UseQueryResult } from "@tanstack/react-query";
 import userService from "./service";
 import User from "../../../types/User";
+import Profile from "../../../types/Profile";
+import Response from "../../../types/Response";
+import useAuthStore from "../../../stores/authStore";
 
 
 
 export const useGetUser = (id: string | number): UseQueryResult<User, Error> => {
     return useQuery({
-        queryKey: ['song', id],
+        queryKey: ['user', id],
         queryFn: () => userService.getUserById(id), 
         staleTime: 5 * 60 * 1000,
         retry: 1,
     });
 };
+
+export const useUpdateProfile = (): UseMutationResult<Response<Profile>, Error, FormData> => {
+    const queryClient = useQueryClient();
+    const {setProfile} = useAuthStore.getState();
+
+    return useMutation({
+        mutationFn: (data: FormData) => userService.updateProfile(data),
+        onSuccess: (res) => {
+            console.log(res);
+            if (res.data){
+                queryClient.invalidateQueries({queryKey: ['user', res.data?.id]});
+                setProfile(res.data);
+            }
+        },
+        onError: (res) => {
+            console.log(res);
+        }
+    });
+} 
