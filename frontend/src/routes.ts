42aa262cd0e0a2b7c7f92b8063e584eceb8d3774
taha jaiti <kyojin.rar@gmail.com>
2025-04-14 12:25:21 +0100

    FEAT: added the route for profile

--- frontend/src/routes.ts
+++ frontend/src/routes.ts
@@ -1,6 +1,7 @@
 import { FC } from 'react';
 import Home from './pages/Home';
-import CreateSongPage from './pages/CreateSongPage';
+import Profile from './pages/Profile';
+import CreateSong from './pages/CreateSong';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
 
@@ -14,8 +15,9 @@ interface RouteConfig {
 const routeConfig: RouteConfig[] = [
     { path: '/', component: Home, layout: 'main', auth: true },
     { path: '/login', component: Login, layout: 'none', auth: false },
-    { path: '/signup', component: Signup, layout: 'none' , auth: false},
-    { path: '/create/song', component: CreateSongPage, layout: 'drum' , auth: true},
+    { path: '/signup', component: Signup, layout: 'none', auth: false },
+    { path: '/profile', component: Profile, layout: 'main', auth: true },
+    { path: '/create/song', component: CreateSong, layout: 'drum', auth: true },
 ];
 
 export default routeConfig;
