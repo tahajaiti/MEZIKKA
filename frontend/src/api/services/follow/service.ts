0522957f39de86d1d768f4d938faf00cbce8f3c2
taha jaiti <kyojin.rar@gmail.com>
2025-04-15 15:26:10 +0100

    FEAT: updated the get follows to support pagination

--- frontend/src/api/services/follow/service.ts
+++ frontend/src/api/services/follow/service.ts
@@ -2,7 +2,7 @@ import api from "../..";
 import { Follow } from "../../../types/Follow";
 import Response from "../../../types/Response";
 
-const getFollows = (id: string | number) => api.get<Response<Follow>>(`/follow/${id}`);
+const getFollows = (id: string | number, pageParam: number = 1) => api.get<Response<Follow>>(`/follow/${id}?page=${pageParam}`);
 const follow = (id: string | number) => api.post<Response<null>>(`/follow/${id}`);
 const unfollow = (id: string | number) => api.delete<Response<null>>(`/follow/${id}`);
 
