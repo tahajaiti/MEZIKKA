e8f4efb39ce7c6eee99a1ec76baeb09d0e7d297b
taha jaiti <kyojin.rar@gmail.com>
2025-04-15 12:02:49 +0100

    FEAT: defined the follow and unfollow methods in the query and service

--- frontend/src/api/services/follow/query.ts
+++ frontend/src/api/services/follow/query.ts
@@ -1,14 +1,45 @@
-import { useQuery, UseQueryResult } from "@tanstack/react-query";
+import { useMutation, UseMutationResult, useQuery, UseQueryResult } from "@tanstack/react-query";
 import followService from "./service";
 import { Follow } from "../../../types/Follow";
 import Response from "../../../types/Response";
 
 
-export const useGetFollows = (): UseQueryResult<Response<Follow>, Error> => {
+export const useGetFollows = (id: string | number): UseQueryResult<Response<Follow>, Error> => {
     return useQuery({
-        queryKey: ['followers'],
-        queryFn: () => followService.getFollows(),
+        queryKey: ['followers', id],
+        queryFn: () => followService.getFollows(id),
         staleTime: 5 * 60 * 1000,
         retry: 1,
     })
+}
+
+export const useFollow = (): UseMutationResult<Response<null>, Error, string | number> => {
+    return useMutation({
+        mutationFn: (id: string | number) => followService.follow(id),
+        onSuccess: (res) => {
+            console.log("Song created successfully", res);
+        },
+        onError: (error) => {
+            console.error("Error creating song", error);
+        },
+    });
+}
+
+export const useUnfollow = (): UseMutationResult<Response<null>, Error, string | number> => {
+    return useMutation({
+        mutationFn: (id: string | number) => followService.unfollow(id),
+        onSuccess: (res) => {
+            console.log("Song created successfully", res);
+        },
+        onError: (error) => {
+            console.error("Error creating song", error);
+        },
+    });
+}
+
+export const useToggleFollow = (isFollowing: boolean) => {
+    const follow = useFollow()
+    const unfollow = useUnfollow()
+
+    return isFollowing ? unfollow : follow
 }
--- frontend/src/api/services/follow/service.ts
+++ frontend/src/api/services/follow/service.ts
@@ -2,10 +2,15 @@ import api from "../..";
 import { Follow } from "../../../types/Follow";
 import Response from "../../../types/Response";
 
-const getFollows = () => api.get<Response<Follow>>('/follows');
+const getFollows = (id: string | number) => api.get<Response<Follow>>(`/follow/${id}`);
+const follow = (id: string | number) => api.post<Response<null>>(`/follow/${id}`);
+const unfollow = (id: string | number) => api.delete<Response<null>>(`/follow/${id}`);
+
 
 const followService = {
     getFollows,
+    follow,
+    unfollow
 }
 
 export default followService;
