927e70125a8e5bd1d2ed0f0eb54b9cb81ea2f2eb
taha jaiti <kyojin.rar@gmail.com>
2025-04-15 12:02:33 +0100

    FEAT: updated the query and service for user to use our custom response

--- frontend/src/api/services/user/query.ts
+++ frontend/src/api/services/user/query.ts
@@ -1,16 +1,16 @@
 import { useMutation, UseMutationResult, useQuery, useQueryClient, UseQueryResult } from "@tanstack/react-query";
 import userService from "./service";
-import User from "../../../types/User";
+import { UserResponse } from "../../../types/User";
 import Profile from "../../../types/Profile";
 import Response from "../../../types/Response";
 import useAuthStore from "../../../stores/authStore";
 
 
 
-export const useGetUser = (id: string | number): UseQueryResult<User, Error> => {
+export const useGetUser = (id: string | number): UseQueryResult<Response<UserResponse>, Error> => {
     return useQuery({
         queryKey: ['user', id],
-        queryFn: () => userService.getUserById(id), 
+        queryFn: () => userService.getUserById(id),
         staleTime: 5 * 60 * 1000,
         retry: 1,
     });
@@ -18,14 +18,14 @@ export const useGetUser = (id: string | number): UseQueryResult<User, Error> => 
 
 export const useUpdateProfile = (): UseMutationResult<Response<Profile>, Error, FormData> => {
     const queryClient = useQueryClient();
-    const {setProfile} = useAuthStore.getState();
+    const { setProfile } = useAuthStore.getState();
 
     return useMutation({
         mutationFn: (data: FormData) => userService.updateProfile(data),
         onSuccess: (res) => {
             console.log(res);
-            if (res.data){
-                queryClient.invalidateQueries({queryKey: ['user', res.data?.id]});
+            if (res.data) {
+                queryClient.invalidateQueries({ queryKey: ['user', res.data?.id] });
                 setProfile(res.data);
             }
         },
--- frontend/src/api/services/user/service.ts
+++ frontend/src/api/services/user/service.ts
@@ -1,10 +1,10 @@
 import api from "../..";
 import Profile from "../../../types/Profile";
 import Response from "../../../types/Response";
-import User from "../../../types/User";
+import { UserResponse } from "../../../types/User";
 
 
-const getUserById = (id: string | number) => api.get<Response<User>>(`/users/${id}`);
+const getUserById = (id: string | number) => api.get<Response<UserResponse>>(`/users/${id}`);
 const updateProfile = (data: FormData) => api.post<Response<Profile>>(`/profile?_method=put`, data);
 
 
