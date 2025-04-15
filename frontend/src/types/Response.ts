2cf33f48fe565fabe2f092fcd56c5509092b6502
taha jaiti <kyojin.rar@gmail.com>
2025-04-15 15:23:26 +0100

    FEAT: feat added the pagination types

--- frontend/src/types/Response.ts
+++ frontend/src/types/Response.ts
@@ -4,4 +4,18 @@ interface Response<T> {
     data?: T;
 }
 
+export interface pagination<T> {
+    current_page: number;
+    data?: T;
+    last_page: number;
+}
+
+export interface PaginateResponse<T> {
+    status: 'success' | 'error';
+    message: string;
+    data: pagination<T>;
+}
+
+
+
 export default Response;
