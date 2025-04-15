5b7ed25900f19001b109e7febf961bc73be899d0
taha jaiti <kyojin.rar@gmail.com>
2025-04-15 15:25:36 +0100

    FEAT: created a profile skeleton

---
+++ frontend/src/components/Page/ProfileSkeleton.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ProfileSkeleton = () => {
+  return (
+    <div className='h-full w-full bg-zinc-500 animate-pulse'>
+
+    </div>
+  )
+}
+
+export default ProfileSkeleton
