a328213c10e21390f0dc4027b3c9c9ebfc8de439
taha jaiti <kyojin.rar@gmail.com>
2025-04-15 20:42:34 +0100

    FEAT: created a simple song card skeleton

---
+++ frontend/src/components/Song/SongCardSkeleton.tsx
@@ -0,0 +1,7 @@
+const SongCardSkeleton = () => {
+    return (
+        <div className="aspect-square animate-pulse bg-zinc-700 rounded-md h-full mb-3"></div>
+    )
+}
+
+export default SongCardSkeleton
