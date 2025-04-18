1eaa02194cbc816d0e5f37c4ab33215413cdaae1
taha jaiti <kyojin.rar@gmail.com>
2025-04-18 15:46:07 +0100

    FEAT: updated the response helper to also implemnt cookies

--- backend/app/Helpers/Res.php
+++ backend/app/Helpers/Res.php
@@ -1,18 +1,27 @@
 <?php
 namespace App\Helpers;
 
+use Symfony\Component\HttpFoundation\Cookie as CookieInstance;
 
-class Res {
-    public static function success($data, $message = 'success', $code = 200): \Illuminate\Http\JsonResponse
+class Res
+{
+    public static function success($data, $message = 'success', $code = 200, $cookie = null): \Illuminate\Http\JsonResponse
     {
-        return response()->json([
+        $response = response()->json([
             'status' => 'success',
             'message' => $message,
             'data' => $data,
         ], $code);
+
+        if ($cookie && $cookie instanceof CookieInstance) {
+            $response->withCookie($cookie);
+        }
+
+        return $response;
     }
 
-    public static function error($message = 'error', $code = 400): \Illuminate\Http\JsonResponse {
+    public static function error($message = 'error', $code = 400): \Illuminate\Http\JsonResponse
+    {
         return response()->json([
             'status' => 'error',
             'message' => $message,
