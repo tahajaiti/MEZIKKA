047f93fbc294ee66073deb4aa9b46456f052b069
taha jaiti <kyojin.rar@gmail.com>
2025-04-18 15:45:52 +0100

    FEAT: updated the authcontroller to add the refresh method and moderized the responses to return cookies

--- backend/app/Http/Controllers/AuthController.php
+++ backend/app/Http/Controllers/AuthController.php
@@ -26,7 +26,7 @@ class AuthController extends Controller
             return Res::error('Failed to register user', 500);
         }
 
-        return Res::success($token, 'User registered successfully', 201);
+        return Res::success($token['data'], 'User registered successfully', 201, $token['refresh_token_cookie']);
     }
 
     public function login(LoginRequest $request)
@@ -37,18 +37,29 @@ class AuthController extends Controller
             return Res::error('Invalid credentials', 401);
         }
 
-        return Res::success($token, 'Logged in successfully');
+        return Res::success($token['data'], 'Logged in successfully', 200, $token['refresh_token_cookie']);
     }
 
     public function logout(Request $request)
     {
-        $token = $request->bearerToken();
+        $token = $this->authService->logout($request);
 
         if (!$token) {
             return Res::error('Token not provided', 401);
         }
 
         // JWT::invalidate($token);
-        return Res::success(null, 'Logged out successfully');
+        return Res::success(null, 'Logged out successfully', $token);
+    }
+
+    public function refresh(Request $request)
+    {
+        $token = $this->authService->refresh($request);
+
+        if (!$token) {
+            return Res::error('Token not provided', 401);
+        }
+
+        return Res::success($token['data'], 'Token refreshed successfully', 200, $token['refresh_token_cookie']);
     }
 }
