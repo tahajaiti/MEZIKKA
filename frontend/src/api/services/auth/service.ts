e82c840f6f07e82d8bc2eaf3c793b7c8d428db5b
taha jaiti <kyojin.rar@gmail.com>
2025-04-18 15:43:35 +0100

    FEAT: added the method for refresh token in the service

--- frontend/src/api/services/auth/service.ts
+++ frontend/src/api/services/auth/service.ts
@@ -6,11 +6,12 @@ import Response from "../../../types/Response";
 const login = (creds: Pick<creds, 'email' | 'password'>) => api.post<Response<AuthData>>('/auth/login', creds);
 const register = (creds: creds) => api.post<Response<AuthData>>('/auth/register', creds);
 const logout = () => api.post<Response<null>>('/auth/logout');
-
+const refreshToken = () => api.post<Response<AuthData>>('/auth/refresh');
 
 const authService = {
     login,
     register,
-    logout
+    logout,
+    refreshToken
 };
 export default authService;
