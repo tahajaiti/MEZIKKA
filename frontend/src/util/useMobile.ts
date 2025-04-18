6455bf5314b48a0e7a836e51eaf6f594bf83279e
taha jaiti <kyojin.rar@gmail.com>
2025-04-18 11:04:28 +0100

    FEAT: created a custom hook to check if the current user is on mobile

---
+++ frontend/src/util/useMobile.ts
@@ -0,0 +1,19 @@
+import { useEffect, useState } from "react"
+
+export function useMobile(breakpoint = 768) {
+    const [isMobile, setIsMobile] = useState(false)
+
+    useEffect(() => {
+        const checkScreenSize = () => {
+            setIsMobile(window.innerWidth < breakpoint)
+        }
+
+        checkScreenSize()
+
+        window.addEventListener("resize", checkScreenSize)
+
+        return () => window.removeEventListener("resize", checkScreenSize)
+    }, [breakpoint])
+
+    return isMobile
+}
