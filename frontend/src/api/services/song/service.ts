0771cba50308ab46c41e525941d835a7d6a6d320
taha jaiti <kyojin.rar@gmail.com>
2025-04-18 09:41:40 +0100

    FIX: fixed the typing the ine song service

--- frontend/src/api/services/song/service.ts
+++ frontend/src/api/services/song/service.ts
@@ -5,7 +5,7 @@ import api, { file } from "../../index";
 
 const createSong = (song: FormData) => api.post<Response<SongData>>('/songs', song);
 const getSongById = (id: string | number) => api.get<Response<SongData>>(`/songs/${id}`);
-const getSongFile = (path: string | number): Promise<ArrayBuffer> => file.get<ArrayBuffer>(`/files/${path}`);
+const getSongFile = (path: string | number): Promise<Blob> => file.get<Blob>(`/files/${path}`);
 const getAll = () => api.get<Response<SongData[]>>('/songs');
 
 const songService = {
