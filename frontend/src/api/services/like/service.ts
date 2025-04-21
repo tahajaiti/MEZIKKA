66258f1731954a45706a6523913f2a7a33de720b
taha jaiti <kyojin.rar@gmail.com>
2025-04-21 09:29:43 +0100

    FEAT: updated the like service to add the getlike count endpoint

--- frontend/src/api/services/like/service.ts
+++ frontend/src/api/services/like/service.ts
@@ -2,11 +2,14 @@ import { LikeData, Likes } from "../../../types/Likes";
 import Response from "../../../types/Response";
 import api from "../../index";
 
+interface CountData {
+    total_likes: number;
+}
 
 const getLikes = () => api.get<Response<Likes>>('/likes');
 const toggleLike = (type: string, id: string | number) => api.post<Response<Likes>>(`/likes/${type}/${id}`);
 const getLike = (type: string, id: string | number) => api.get<Response<LikeData>>(`/likes/${type}/${id}`);
-const getLikeCount = (type: string, id: string | number) => api.get<Response<Likes>>(`/likes/${type}/${id}/count`);
+const getLikeCount = (type: string, id: string | number) => api.get<Response<CountData>>(`/likes/${type}/${id}/count`);
 
 const likeService = {
     getLikes,
