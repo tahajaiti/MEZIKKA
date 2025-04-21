fdfa0857aab0b00a49acbf49a8a12f0ee880969d
taha jaiti <kyojin.rar@gmail.com>
2025-04-21 10:00:06 +0100

    FEAT: implemnted the like loader inside the mainlayout

--- frontend/src/layouts/MainLayout.tsx
+++ frontend/src/layouts/MainLayout.tsx
@@ -1,13 +1,15 @@
 import Header from '../components/Global/Header';
+import LikeLoader from '../components/Like/LikeLoader';
 import Player from '../components/Player/Player';
 import { Outlet } from 'react-router';
 
 const MainLayout: React.FC = () => {
   return (
     <div className="h-screen w-screen overflow-hidden flex flex-col">
+      <LikeLoader />
       <Header />
       <main className='h-full w-full overflow-auto'>
-        <Outlet/>
+        <Outlet />
       </main>
       <Player />
     </div>
