bb3d174eaac05722584326f60e00b61b38d8d60f
taha jaiti <kyojin.rar@gmail.com>
2025-04-21 16:21:24 +0100

    FEAT: fixed the likes types

--- frontend/src/types/Likes.ts
+++ frontend/src/types/Likes.ts
@@ -1,13 +1,6 @@
-import SongData from "./Song";
-
 export interface LikesGet {
     songs: number[];
     playlists: number[];
-}
-
-export interface Likes {
-    songs?: SongData[];
-    playlists?: SongData[];
 }
 
 
