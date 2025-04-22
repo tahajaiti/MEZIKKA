56627bfa85010a75849f6cb955dad3194087fab2
taha jaiti <kyojin.rar@gmail.com>
2025-04-22 10:54:27 +0100

    FEAT: implemnted the user search service and query

--- frontend/src/api/services/search/query.ts
+++ frontend/src/api/services/search/query.ts
@@ -4,14 +4,20 @@ import searchService from "./service";
 
 
 
-const useSearch = (query: string, enabled = true) => {
+export const useSearch = (query: string, enabled = true) => {
     return useQuery({
-        queryKey: ["search", query],
+        queryKey: ["search", "songs", query],
         queryFn: () => searchService.search(query),
         enabled: enabled && query.length > 0,
         staleTime: 1000 * 60 * 5,
     });
 }
 
-
-export default useSearch;
+export const useUserSearch = (query: string, enabled = true) => {
+    return useQuery({
+        queryKey: ["search", "users", query],
+        queryFn: () => searchService.userSearch(query),
+        enabled: enabled && query.length > 0,
+        staleTime: 1000 * 60 * 5,
+    });
+}
--- frontend/src/api/services/search/service.ts
+++ frontend/src/api/services/search/service.ts
@@ -1,14 +1,16 @@
 import api from "../..";
 import Response from "../../../types/Response";
 import SongData from "../../../types/Song";
+import User from "../../../types/User";
 
 
 
 const search = (query: string) => api.get<Response<SongData[]>>(`/search?q=${query}`);
-
+const userSearch = (query: string) => api.get<Response<User[]>>(`/search/user?q=${query}`);
 
 const searchService = {
-    search
+    search,
+    userSearch
 };
 
 export default searchService;
