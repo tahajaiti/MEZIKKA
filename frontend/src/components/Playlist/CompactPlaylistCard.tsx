8c9d83e912732c060ce015931911762e8c61cfe8
taha jaiti <kyojin.rar@gmail.com>
2025-04-22 17:06:43 +0100

    FEAT: created a simple compact playlist card

---
+++ frontend/src/components/Playlist/CompactPlaylistCard.tsx
@@ -0,0 +1,27 @@
+import { PlaylistData } from "../../types/Playlist"
+import { formatUrl } from "../../util/Formatters";
+
+
+interface props {
+    playlist: PlaylistData;
+}
+
+const CompactPlaylistCard = ({ playlist }: props) => {
+    return (
+        <div
+            className="flex items-center bg-zinc-900 rounded-md p-2 cursor-pointer hover:bg-zinc-800 transition-all border border-zinc-800">
+            <div className="relative w-16 h-16 mr-3 flex-shrink-0">
+                <img
+                    src={formatUrl(playlist.cover)}
+                    alt={`${playlist.title} cover`}
+                    className="w-full h-full object-cover rounded"
+                />
+
+            </div>
+
+            <h4 className="font-medium text-white text-sm truncate">{playlist.title}</h4>
+        </div>
+    )
+}
+
+export default CompactPlaylistCard
