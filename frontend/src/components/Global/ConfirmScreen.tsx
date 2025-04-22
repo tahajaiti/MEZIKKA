2a7a2b345865ab84bd7ac440d5662a2020ad5653
taha jaiti <kyojin.rar@gmail.com>
2025-04-22 15:54:16 +0100

    FEAT: improved the design of the confirmation screen

--- frontend/src/components/Global/ConfirmScreen.tsx
+++ frontend/src/components/Global/ConfirmScreen.tsx
@@ -12,19 +12,19 @@ const ConfirmScreen = () => {
 
     return (
         <div className="fixed inset-0 bg-black/50 flex items-center justify-center z-[100]">
-            <div className="bg-white p-5 rounded-md shadow-lg">
+            <div className="bg-zinc-900 border border-zinc-800 p-5 rounded-md shadow-lg">
                 <h2 className="text-lg font-bold mb-4">Confirmation</h2>
                 <p className="mb-4">{message}</p>
                 <div className="flex justify-end gap-2">
                     <button
                         onClick={handleConfirm}
-                        className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600"
+                        className="bg-red-500 text-white px-4 py-2 rounded-sm font-bold cursor-pointer shadow-md shadow-red-500/30 hover:bg-red-600"
                     >
                         Confirm
                     </button>
                     <button
                         onClick={() => useConfirmStore.setState({ isVisible: false })}
-                        className="bg-gray-300 text-gray-700 px-4 py-2 rounded-md hover:bg-gray-400"
+                        className="bg-zinc-500 px-4 py-2 rounded-sm font-bold hover:bg-zinc-600 cursor-pointer"
                     >
                         Cancel
                     </button>
