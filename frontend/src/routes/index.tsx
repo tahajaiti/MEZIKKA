5e9acfa399136da4fc506c3591426ff5dc97c1d9
taha jaiti <kyojin.rar@gmail.com>
2025-04-24 09:59:19 +0100

    FIX: fixed  an issue with the routes to put the aestrisk at the end of routing

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -20,7 +20,6 @@ export interface Route {
 }
 
 const routes: Route[] = [
-    { path: '*', component: NotFound, layout: null, auth: false },
     { path: '/', component: Home, layout: MainLayout, auth: true },
     { path: '/login', component: Login, layout: null, auth: false },
     { path: '/signup', component: Signup, layout: null, auth: false },
@@ -29,8 +28,9 @@ const routes: Route[] = [
     { path: '/song', component: CreateSong, layout: DrumLayout, auth: true },
     { path: '/song/:id', component: Song, layout: MainLayout, auth: true },
     { path: '/search', component: Search, layout: MainLayout, auth: true },
-    { path: '/playlist', component: Playlist, layout: MainLayout, auth: true },
     { path: '/playlist/:id', component: PlaylistDetails, layout: MainLayout, auth: true },
+    { path: '/playlist', component: Playlist, layout: MainLayout, auth: true },
+    { path: '*', component: NotFound, layout: null, auth: false },
 ];
 
 export default routes;
