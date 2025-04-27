b192f424ed5d9b910839f381382b628812e8a684
taha jaiti <kyojin.rar@gmail.com>
2025-04-27 17:16:36 +0100

    FEAT: change AnimatePresence mode from "wait" to "sync" for improved route transitions

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -25,7 +25,7 @@ const App = () => {
   return (
     <QueryClientProvider client={queryClient}>
       <NavigationHandler />
-      <AnimatePresence mode="wait">
+      <AnimatePresence mode="sync">
         <Routes>
           {render(routes)}
         </Routes>
