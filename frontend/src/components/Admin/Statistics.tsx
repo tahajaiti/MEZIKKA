de3fadb269ec586bc6f7e1f0f91ba3d06370e63f
taha jaiti <kyojin.rar@gmail.com>
2025-04-27 20:49:54 +0100

    FEAT: refactor Statistics component to use individual hooks for user, song, and playlist stats with period selection

--- frontend/src/components/Admin/Statistics.tsx
+++ frontend/src/components/Admin/Statistics.tsx
@@ -1,52 +1,72 @@
 import { Disc, List, Tag, User } from 'lucide-react'
 import StatCard from './StatCard'
-import { useGetStats } from '../../api/services/stats/query'
+import { useGetPlaylistStats, useGetSongStats, useGetUserStats } from '../../api/services/stats/query'
+import { useState } from 'react';
 
 const Statistics = () => {
-    const { data, isPending } = useGetStats();
+    const [userPeriod, setUserPeriod] = useState(7);
+    const [songPeriod, setSongPeriod] = useState(7);
+    const [playlistPeriod, setPlaylistPeriod] = useState(7);
+    const { data: userData, isPending: userPending, refetch: userRefetch } = useGetUserStats(userPeriod);
+    const { data: songData, isPending: songPending, refetch: songRefetch } = useGetSongStats(songPeriod);
+    const { data: playlistData, isPending: playlistPending, refetch: playlistRefetch } = useGetPlaylistStats(playlistPeriod);
 
-
-    if (isPending || !data?.data) {
-        return (
-            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8'>
-                <StatCard
-                    title='Total Users'
-                    value='Loading...'
-                    icon={<User />} />
-                <StatCard
-                    title='Total Songs'
-                    value='Loading...'
-                    icon={<Disc />} />
-                <StatCard
-                    title='Total Playlists'
-                    value='Loading...'
-                    icon={<List />} />
-                <StatCard
-                    title='Total Genres'
-                    value='Loading...'
-                    icon={<Tag />} />
-            </div>
-        )
+    const handleUserStats = (period: number) => {
+        setUserPeriod(period);
+        userRefetch();
     }
 
+    const handleSongStats = (period: number) => {
+        setSongPeriod(period);
+        songRefetch();
+    }
+    const handlePlaylistStats = (period: number) => {
+        setPlaylistPeriod(period);
+        playlistRefetch();
+    }
+
+
     return (
-        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8'>
+        <div className='grid grid-cols-1 lg:grid-cols-2 gap-4 sm:gap-6'>
             <StatCard
                 title='Total Users'
-                value={data.data.total_users}
-                icon={<User />} />
+                value={userData?.data?.total ?? 0}
+                growth={userData?.data?.growth ?? 0}
+                isPending={userPending}
+                handler={handleUserStats}
+                setter={setUserPeriod}
+                icon={<User className="w-5 h-5" />}
+            />
+
             <StatCard
                 title='Total Songs'
-                value={data.data.total_songs}
-                icon={<Disc />} />
+                value={songData?.data?.total ?? 0}
+                growth={songData?.data?.growth ?? 0}
+                isPending={songPending}
+                handler={handleSongStats}
+                setter={setSongPeriod}
+                icon={<Disc className="w-5 h-5" />}
+            />
+
             <StatCard
-                title='Total Playlists'
-                value={data.data.total_playlists}
-                icon={<List />} />
+                title='Genres'
+                value={100}
+                growth={100}
+                isPending={false}
+                handler={handleUserStats}
+                setter={setUserPeriod}
+                icon={<Tag className="w-5 h-5" />}
+            />
+
             <StatCard
-                title='Total Genres'
-                value={data.data.total_genres}
-                icon={<Tag />} />
+                title='Playlists'
+                value={playlistData?.data?.total ?? 0}
+                growth={playlistData?.data?.growth ?? 0}
+                isPending={playlistPending}
+                handler={handlePlaylistStats}
+                setter={setPlaylistPeriod}
+                icon={<List className="w-5 h-5" />}
+            />
         </div>
     )
 }
