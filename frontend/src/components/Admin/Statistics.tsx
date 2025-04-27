3b33d07fbdbc5357672612ccd07896578bf27b18
taha jaiti <kyojin.rar@gmail.com>
2025-04-27 18:58:09 +0100

    FEAT: enhance Statistics component to fetch and display dynamic stats data

--- frontend/src/components/Admin/Statistics.tsx
+++ frontend/src/components/Admin/Statistics.tsx
@@ -1,24 +1,51 @@
 import { Disc, List, Tag, User } from 'lucide-react'
 import StatCard from './StatCard'
+import { useGetStats } from '../../api/services/stats/query'
 
 const Statistics = () => {
+    const { data, isPending } = useGetStats();
+
+
+    if (isPending || !data?.data) {
+        return (
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8'>
+                <StatCard
+                    title='Total Users'
+                    value='Loading...'
+                    icon={<User />} />
+                <StatCard
+                    title='Total Songs'
+                    value='Loading...'
+                    icon={<Disc />} />
+                <StatCard
+                    title='Total Playlists'
+                    value='Loading...'
+                    icon={<List />} />
+                <StatCard
+                    title='Total Genres'
+                    value='Loading...'
+                    icon={<Tag />} />
+            </div>
+        )
+    }
+
     return (
         <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8'>
             <StatCard
                 title='Total Users'
-                value='1,234'
+                value={data.data.total_users}
                 icon={<User />} />
             <StatCard
                 title='Total Songs'
-                value='1,234'
+                value={data.data.total_songs}
                 icon={<Disc />} />
             <StatCard
                 title='Total Playlists'
-                value='1,234'
+                value={data.data.total_playlists}
                 icon={<List />} />
             <StatCard
                 title='Total Genres'
-                value='1,234'
+                value={data.data.total_genres}
                 icon={<Tag />} />
         </div>
     )
