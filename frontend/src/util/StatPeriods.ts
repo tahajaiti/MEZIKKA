d069a98567e30b987fab09adbd05a3551e83a3dc
taha jaiti <kyojin.rar@gmail.com>
2025-04-27 20:49:47 +0100

    FEAT: add utility for defining time periods for statistics

---
+++ frontend/src/util/StatPeriods.ts
@@ -0,0 +1,11 @@
+
+
+
+export const periods = [
+    { label: '1 Day', value: 1 },
+    { label: '7 Days', value: 7 },
+    { label: '30 Days', value: 30 },
+    { label: '1 Year', value: 365 },
+    { label: 'All Time', value: 0 },
+]
+
