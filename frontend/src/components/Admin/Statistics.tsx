669dc17d31a0bbc7a75626dfb614293971846a86
taha jaiti <kyojin.rar@gmail.com>
2025-04-28 11:39:47 +0100

    FEAT: add TopCard component for displaying top genres and songs statistics

--- frontend/src/components/Admin/Statistics.tsx
+++ frontend/src/components/Admin/Statistics.tsx
@@ -1,7 +1,8 @@
 import { Disc, Heart, List, User } from 'lucide-react'
 import StatCard from './StatCard'
-import { useGetLikeStats, useGetPlaylistStats, useGetSongStats, useGetUserStats } from '../../api/services/stats/query'
+import { useGetLikeStats, useGetPlaylistStats, useGetSongStats, useGetTopGenres, useGetTopSongs, useGetUserStats } from '../../api/services/stats/query'
 import { useState } from 'react';
+import TopCard from './TopCard';
 
 const Statistics = () => {
     const [userPeriod, setUserPeriod] = useState(1);
@@ -12,6 +13,9 @@ const Statistics = () => {
     const { data: userData, isPending: userPending, refetch: userRefetch } = useGetUserStats(userPeriod);
     const { data: songData, isPending: songPending, refetch: songRefetch } = useGetSongStats(songPeriod);
     const { data: playlistData, isPending: playlistPending, refetch: playlistRefetch } = useGetPlaylistStats(playlistPeriod);
+
+    const { data: topGenresData, isPending: topGenresPending } = useGetTopGenres();
+    const { data: topSongsData, isPending: topSongsPending } = useGetTopSongs();
 
     const handleUserStats = (period: number) => {
         setUserPeriod(period);
@@ -34,46 +38,53 @@ const Statistics = () => {
 
 
     return (
-        <div className='grid grid-cols-1 lg:grid-cols-2 gap-4 sm:gap-6'>
-            <StatCard
-                title='Total Users'
-                value={userData?.data?.total ?? 0}
-                growth={userData?.data?.growth ?? 0}
-                isPending={userPending}
-                handler={handleUserStats}
-                setter={setUserPeriod}
-                icon={<User className="w-5 h-5" />}
-            />
+        <div className='flex flex-col gap-6 px-6'>
+            <div className='grid grid-cols-1 lg:grid-cols-2 gap-4 sm:gap-6'>
+                <StatCard
+                    title='Total Users'
+                    value={userData?.data?.total ?? 0}
+                    growth={userData?.data?.growth ?? 0}
+                    isPending={userPending}
+                    handler={handleUserStats}
+                    setter={setUserPeriod}
+                    icon={<User className="w-5 h-5" />}
+                />
 
-            <StatCard
-                title='Total Songs'
-                value={songData?.data?.total ?? 0}
-                growth={songData?.data?.growth ?? 0}
-                isPending={songPending}
-                handler={handleSongStats}
-                setter={setSongPeriod}
-                icon={<Disc className="w-5 h-5" />}
-            />
+                <StatCard
+                    title='Total Songs'
+                    value={songData?.data?.total ?? 0}
+                    growth={songData?.data?.growth ?? 0}
+                    isPending={songPending}
+                    handler={handleSongStats}
+                    setter={setSongPeriod}
+                    icon={<Disc className="w-5 h-5" />}
+                />
 
-            <StatCard
-                title='Likes'
-                value={likeData?.data?.total ?? 0}
-                growth={likeData?.data?.growth ?? 0}
-                isPending={likePending}
-                handler={handleLikeStats}
-                setter={setLikePeriod}
-                icon={<Heart className="w-5 h-5" />}
-            />
+                <StatCard
+                    title='Likes'
+                    value={likeData?.data?.total ?? 0}
+                    growth={likeData?.data?.growth ?? 0}
+                    isPending={likePending}
+                    handler={handleLikeStats}
+                    setter={setLikePeriod}
+                    icon={<Heart className="w-5 h-5" />}
+                />
 
-            <StatCard
-                title='Playlists'
-                value={playlistData?.data?.total ?? 0}
-                growth={playlistData?.data?.growth ?? 0}
-                isPending={playlistPending}
-                handler={handlePlaylistStats}
-                setter={setPlaylistPeriod}
-                icon={<List className="w-5 h-5" />}
-            />
+                <StatCard
+                    title='Playlists'
+                    value={playlistData?.data?.total ?? 0}
+                    growth={playlistData?.data?.growth ?? 0}
+                    isPending={playlistPending}
+                    handler={handlePlaylistStats}
+                    setter={setPlaylistPeriod}
+                    icon={<List className="w-5 h-5" />}
+                />
+            </div>
+
+            <div className='grid grid-cols-1 lg:grid-cols-2 gap-4 sm:gap-6'>
+                <TopCard type='genres' data={topGenresData?.data ?? []} isLoading={topGenresPending}/>
+                <TopCard type='songs' data={topSongsData?.data ?? []} isLoading={topSongsPending} />
+            </div>
         </div>
     )
 }
---
+++ frontend/src/components/Admin/TopCard.tsx
@@ -0,0 +1,62 @@
+import React from 'react';
+import { TopGenres, TopSongs } from '../../api/services/stats/service';
+import { Disc, Tag } from 'lucide-react';
+
+interface TopCardProps {
+    type: 'genres' | 'songs';
+    data: TopGenres[] | TopSongs[];
+    isLoading: boolean;
+}
+
+const TopCard: React.FC<TopCardProps> = ({ type, data, isLoading }) => {
+    return (
+        <div className='bg-zinc-900/50 backdrop-blur-xl rounded-xl p-6 border border-zinc-800 shadow-lg 
+                        transition-all hover:border-zinc-700 hover:bg-zinc-900/70'>
+
+            <h2 className="text-xl font-bold mb-5 capitalize flex items-center">
+                <span className="text-red-500 mr-2">
+                    {type === 'songs' ? <Disc /> : <Tag />}
+                </span>
+                Top 5 {type}
+            </h2>
+
+            {isLoading ? (
+                <div className="flex flex-col space-y-3 animate-pulse">
+                    <div className="h-34 bg-zinc-800 rounded-md w-full"></div>
+                </div>
+            ) : (
+                <ul className="space-y-3">
+                    {data.length > 0 ? (
+                        data.map((item, index) => {
+                            const itemName = type === 'songs' ? (item as TopSongs).name : (item as TopGenres).name;
+                            const value = type === 'songs'
+                                ? (item as TopSongs).likes_count
+                                : (item as TopGenres).songs_count;
+
+                            return (
+                                <li
+                                    key={index}
+                                    className="flex items-center justify-between p-3 rounded-lg bg-zinc-800/50 hover:bg-zinc-800 transition-all"
+                                >
+                                    <div className="flex items-center">
+                                        <span className="text-zinc-400 mr-3">{index + 1}</span>
+                                        <span className="text-zinc-200 font-medium">{itemName}</span>
+                                    </div>
+                                    <div className="text-sm px-2 py-1 rounded-full bg-red-500 text-white">
+                                        {type === 'songs' ? `${value} likes` : `${value} Songs`}
+                                    </div>
+                                </li>
+                            );
+                        })
+                    ) : (
+                        <li className="text-zinc-400 text-center">
+                            No data available
+                        </li>
+                    )}
+                </ul>
+            )}
+        </div>
+    );
+};
+
+export default TopCard;
