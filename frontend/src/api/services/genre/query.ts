365bba3ab1ef1c0f91a78ec516e155130dc260fd
taha jaiti <kyojin.rar@gmail.com>
2025-04-28 12:09:12 +0100

    FEAT: add getPaginated method to genreService and update useGetGenres hook

--- frontend/src/api/services/genre/query.ts
+++ frontend/src/api/services/genre/query.ts
@@ -1,18 +1,21 @@
-import { useQuery, UseQueryResult } from "@tanstack/react-query";
-import Response from "../../../types/Response";
-import Genre from "../../../types/Genre";
+import { useMutation, useQuery } from "@tanstack/react-query";
 import genreService from "./service";
 
 
 
-export const useGetGenres = (): UseQueryResult<
-    Response<Genre[]>,
-    Error
-> => {
-
+export const useGetGenres = () => {
     return useQuery({
         queryKey: [`genres-${new Date()}`],
         queryFn: genreService.getAll,
+        staleTime: 5 * 60 * 1000,
+        retry: 1
+    })
+}
+
+export const useGetPaginatedGenres = (page: number) => {
+    return useQuery({
+        queryKey: [`genres-${page}`],
+        queryFn: () => genreService.getPaginated(page),
         staleTime: 5 * 60 * 1000,
         retry: 1
     })
@@ -24,5 +27,17 @@ export const useGetGenreImg = (genre: string) => {
         queryFn: () => genreService.getImg(genre),
         staleTime: 10 * 60 * 1000,
         retry: 1
+    })
+}
+
+export const useCreateGenre = () => {
+    return useMutation({
+        mutationFn: genreService.createGenre,
+        onError: (error) => {
+            console.error("Error creating genre:", error);
+        },
+        onSuccess: () => {
+            console.log("Genre created successfully");
+        }
     })
 }
--- frontend/src/api/services/genre/service.ts
+++ frontend/src/api/services/genre/service.ts
@@ -1,16 +1,21 @@
 import api from "../..";
 import Genre from "../../../types/Genre";
-import Response from "../../../types/Response";
+import Response, { PaginateResponse } from "../../../types/Response";
 
 
 
 
 
 const getAll = () => api.get<Response<Genre[]>>('/genres');
+const getPaginated = (page: number) => api.get<PaginateResponse<Genre[]>>(`/genres?page=${page}`);
 const getImg = (genre: string) => api.get<Response<string>>(`/genres/image/${genre}`);
+
+const createGenre = (name: string) => api.post<Response<Genre>>('/genres', { name });
 
 const genreService = {
     getAll,
+    getPaginated,
+    createGenre,
     getImg
 }
 
